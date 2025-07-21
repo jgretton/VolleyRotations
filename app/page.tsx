eaf8e360850ef6f8fcd1c7cfae37c4b756cd6fb0
Joshua Gretton <jb.gretton@googlemail.com>
2025-07-21 16:07:32 +0100

    Data being passed to front end
    Created a get all data function so that i can display all categories and content links on the front end. Basic layout introduced

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,56 @@
+import { getAllContent } from "@/lib/content";
+import PageHeader from "./components/PageHeader";
+import { g, li } from "motion/react-client";
+import Link from "next/link";
+
 export default function Home() {
+	const allContent = getAllContent();
+
 	return (
 		<main className="min-h-screen max-w-7xl mx-auto px-4 ">
-			<section className="pt-20">
-				<h1 className="text-center text-[min(10vw,5rem)] font-light">
-					VolleyRotations
-				</h1>
-				<p className="text-center mt-10 text-balance  mx-auto text-xl font-light leading-relaxed">
+			<section className="mt-20">
+				<PageHeader title={"VolleyRotations"}>
 					 VolleyRotations is your go-to resource for understanding the
 					fundamentals of volleyball court positions. Whether you’re a beginner
 					looking to grasp the basics or an intermediate player aiming to refine
 					your skills, our site is designed to help you navigate the intricacies
-					of the game.
-				</p>
+					of the game.{" "}
+				</PageHeader>
+			</section>
+
+			<section className="grid grid-cols-1 gap-10 mt-20">
+				{allContent ? (
+					allContent.categories.map((category) => {
+						console.log(category);
+						return (
+							<div className="" key={category}>
+								<h2 className="rounded-full py-3 px-6 text-lg/6 font-semibold text-black focus:outline-none bg-black/10 inline-block ">
+									{category}
+								</h2>
+								<ul className="flex flex-col md:flex-row p-3">
+									{allContent.content[category].map((content) => {
+										return (
+											<li
+												key={content.slug}
+												className="relative rounded-md p-3 md:w-1/3 w-full text-sm/6 transition hover:bg-black/5 "
+											>
+												<Link
+													href={`/${category}/${content.slug}`}
+													className="font-semibold text-black"
+												>
+													<div className="h-60 w-full bg-red-100 rounded-md mb-2"></div>
+													{content.title}
+												</Link>
+											</li>
+										);
+									})}
+								</ul>
+							</div>
+						);
+					})
+				) : (
+					<div>No guide exists</div>
+				)}
 			</section>
 		</main>
 	);
--- lib/content.ts
+++ lib/content.ts
@@ -10,22 +10,59 @@ type Category = {
 	data: object;
 	content: object;
 };
+type ReturnData = {
+	categories: string[];
+	content: {
+		[categoryName: string]: {
+			slug: string;
+			title: string;
+			description: string;
+		}[];
+	};
+};
 
-export const getAllContent = () => {
+export const getAllContent = (): ReturnData | null => {
 	if (!fs.existsSync(contentDirectory)) {
 		console.warn(`Directory ${contentDirectory} does not exist`);
 		return null;
 	}
 
 	const allFileContent = fs.readdirSync(contentDirectory);
-	return allFileContent.map((category) => {
-		return { [category]: getContentByCategory(category) };
+	const returnData: ReturnData = {
+		categories: allFileContent,
+		content: {},
+	};
+	allFileContent.forEach((category) => {
+		const categoryPath = path.join(contentDirectory, category);
+		const fileNames = fs.readdirSync(categoryPath);
+
+		const fileData = fileNames
+			.filter((fileName: string) => fileName.endsWith("yaml"))
+			.map((fileName: string) => {
+				const slug = fileName.replace(/\.(yaml)$/, "");
+				const fullFilePath = path.join(categoryPath, fileName);
+				const fileContents = fs.readFileSync(fullFilePath, "utf-8");
+				const { data } = matter(fileContents);
+
+				const { title, description } = data;
+
+				return {
+					slug,
+					title,
+					description,
+				};
+			});
+
+		returnData.content[category] = fileData;
 	});
+
+	return returnData;
 };
 
 export const getContentByCategory = (category: string): Category[] | null => {
 	if (!category) return null;
 	const guidePath = path.join(contentDirectory, category);
+
 	if (!fs.existsSync(guidePath)) {
 		console.warn(`Directory ${guidePath} does not exist`);
 		return null;
